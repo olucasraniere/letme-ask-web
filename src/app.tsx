a0e3edbf3def1361ce4ce4ce08d145a4cf5681e6
lucas_raniere <olucas.raniere@gmail.com>
2025-07-09 17:03:03 -0300

    chore: set up TailwindCSS and ShadUI
    - Installed TailwindCSS and @tailwindcss/vite.
    - Updated vite.config.ts to include TailwindCSS configurations.
    - Imported TailwindCSS in src/index.css.
    - Configured project to support ShadUI:
      - Edited tsconfig.json and tsconfig.app.json.
      - Installed @types/node as a dev dependency.
      - Updated vite.config.ts to support ShadUI.

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import { Wand } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Button>Hello</Button>
+      <br />
+      <Button variant={'secondary'}>Hello</Button>
+      <Button variant={'outline'}>Hello</Button>
+      <Button variant={'ghost'}>Hello</Button>
+      <Button variant={'destructive'}>Hello</Button>
+      <Button variant={'link'}>Hello</Button>
+      <br />
+      <Button size={'sm'}>Hello</Button>
+      <Button size={'lg'}>Hello</Button>
+      <Button size={'icon'}>
+        <Wand />
+      </Button>
+    </div>
+  )
 }
